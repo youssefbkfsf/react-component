9f207d57d5c71012e018bda77a3df56dbb8aaf45
unknown <youssefbenkhadija2007@gmail.com>
2024-01-25 11:55:00 +0100

    first commit

---
+++ react component.js
@@ -0,0 +1,15 @@
+// Import the React library
+import React from 'react';
+
+// Define the HelloWorld component using JSX
+const HelloWorld = () => {
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+      <p>This is a simple React component written in JSX.</p>
+    </div>
+  );
+};
+
+// Export the HelloWorld component for use in other parts of your application
+export default HelloWorld;
